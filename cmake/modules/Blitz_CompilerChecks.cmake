5663e03f3dcdd9e309b86b205315fb360ae33e84
Michele Rosso <mrosso@lbl.gov>
2019-02-04 08:52:49 -0800

    More CMake stuff

--- cmake/modules/Blitz_CompilerChecks.cmake
+++ cmake/modules/Blitz_CompilerChecks.cmake
@@ -1,18 +1,15 @@
 include(CheckCXXSourceCompiles)
 
-function (cxx_namespaces result)
+macro (cxx_namespaces)
    message(STATUS "Checking whether the compiler implements namespaces" )
    check_cxx_source_compiles(
       "namespace Outer { namespace Inner { int i = 0; }}
        int main(){using namespace Outer::Inner; return i;}"
-      support_namespaces
+      HAVE_NAMESPACES
       )
-   if (support_namespaces)
-      set(${result} 1 PARENT_SCOPE)
-   endif ()
-endfunction ()
+endmacro ()
 
-function (cxx_have_bool result)
+macro (cxx_have_bool)
    message(STATUS "Checking whether the compiler recognizes "
       "bool as a built-in type")
    check_cxx_source_compiles(
@@ -20,40 +17,32 @@ function (cxx_have_bool result)
        int f(char x){return 1;}
        int f(bool x){return 1;}     
        int main() {return 0;}"
-      support_bool
+      HAVE_BOOL
       )
-   if (support_bool)
-      set(${result} 1 PARENT_SCOPE) 
-   endif ()
-endfunction ()
+endmacro ()
 
 
-function (cxx_have_complex result)
-   if(NOT HAVE_NAMESPACES)
-      return()
+macro (cxx_have_complex)
+   if (HAVE_NAMESPACES)
+      set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    endif ()
    message(STATUS "Checking whether the compiler has complex<T>")
-   set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    check_cxx_source_compiles(
       "#include <complex>
        #ifdef HAVE_NAMESPACES
        using namespace std;
        #endif     
        int main() {complex<float> a; complex<double> b; return 0;}"
-      support_complex
+      HAVE_COMPLEX
       )
-   if (support_complex)
-      set(${result} 1 PARENT_SCOPE)  
-   endif ()
-endfunction ()
+endmacro ()
 
 
-function (cxx_have_complex_fcns result)
-   if(NOT HAVE_NAMESPACES)
-      return()
+macro (cxx_have_complex_fcns)
+   if (HAVE_NAMESPACES)
+      set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    endif ()
    message(STATUS "Checking whether the compiler has standard complex<T> functions")
-   set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    check_cxx_source_compiles(
       "#include <complex>
        #ifdef HAVE_NAMESPACES
@@ -61,20 +50,15 @@ function (cxx_have_complex_fcns result)
        #endif     
        int main() { complex<double> x(1.0, 1.0); real(x); imag(x); 
        abs(x); arg(x); norm(x); conj(x); polar(1.0,1.0); return 0;}"
-      support_complex_fcns
+      HAVE_COMPLEX_FCNS
       )
-   if (support_complex_fcns)
-      set(${result} 1 PARENT_SCOPE)  
-   endif ()
-endfunction ()
+endmacro ()
 
-function (cxx_have_complex_math1 result)
-   if(NOT HAVE_NAMESPACES)
-      return()
+macro (cxx_have_complex_math1)
+   if (HAVE_NAMESPACES)
+      set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    endif ()
-   message("HERE")
    message(STATUS "Checking whether the compiler has complex math functions")
-   set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    check_cxx_source_compiles(
       "#include <complex>
        #ifdef HAVE_NAMESPACES
@@ -84,19 +68,16 @@ function (cxx_have_complex_math1 result)
        cos(x); cosh(x); exp(x); log(x); pow(x,1); pow(x,double(2.0));
        pow(x, y); pow(double(2.0), x); sin(x); sinh(x); sqrt(x); tan(x);
        tanh(x); return 0;}"
-      support_complex_math1
+      HAVE_COMPLEX_MATH1
       )
-   if (support_complex_math1)
-      set(${result} 1 PARENT_SCOPE)
-   endif ()
-endfunction ()
+endmacro ()
 
-function (cxx_have_complex_math2 result)
-   if(NOT HAVE_NAMESPACES)
-      return()
+macro (cxx_have_complex_math2)
+   if (HAVE_NAMESPACES)
+      set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    endif ()
    message(STATUS "Checking whether the compiler has more complex math functions")
-   set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
+
    check_cxx_source_compiles(
       "#include <complex>
        #ifdef HAVE_NAMESPACES
@@ -105,39 +86,28 @@ function (cxx_have_complex_math2 result)
        int main() {complex<double> x(1.0, 1.0), y(1.0, 1.0);
        cos(x); asin(x); atan(x); atan2(x,y); atan2(x, double(3.0));
        atan2(double(3.0), x); log10(x); return 0;}"
-      support_complex_math2
+      HAVE_COMPLEX_MATH2
       )
-   if (support_complex_math2)
-      set(${result} 1 PARENT_SCOPE)
-   endif ()
-endfunction ()
+endmacro ()
 
-function (cxx_have_complex_math_in_namespace_std result)
-   if(NOT HAVE_NAMESPACES)
-      return()
+macro (cxx_have_complex_math_in_namespace_std)
+   if (HAVE_NAMESPACES)
+      set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    endif ()
    message(STATUS "Checking whether the compiler has more complex math functions")
-   set(CMAKE_REQUIRED_DEFINITIONS -DHAVE_NAMESPACES)
    check_cxx_source_compiles(
       "#include <complex>
        namespace S { using namespace std;
-       complex<float> pow(complex<float> x, complex<float> y)
-       { return std::pow(x,y); }};
-       int main {using namespace S; complex<float> x = 1.0, y = 1.0; S::pow(x,y); return 0;}"
-      support_complex_math_in_std
+       complex<float> pow(complex<float> x, complex<float> y){ return std::pow(x,y); }};
+       int main () {using namespace S; complex<float> x = 1.0, y = 1.0; S::pow(x,y); return 0;}"
+      HAVE_COMPLEX_MATH_IN_NAMESPACE_STD
       )
-   if (support_complex_math_in_std)
-      set(${result} 1 PARENT_SCOPE)
-   endif ()
-endfunction()
+endmacro()
 
-function (cxx_have_const_cast result)
+macro (cxx_have_const_cast)
    message(STATUS "Checking whether the compiler supports const_cast<>")
    check_cxx_source_compiles(
       "int main() {int x = 0;const int& y = x;int& z = const_cast<int&>(y);return z;}"
-      support_const_cast
+      HAVE_CONST_CAST
       )
-   if (support_const_cast)
-      set(${result} 1 PARENT_SCOPE)
-   endif ()
-endfunction()
+endmacro()
